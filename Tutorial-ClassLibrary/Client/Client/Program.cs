79df4e64f216763d3c4233137bf464f215ac5076
Jacky Eastmoon <jacky.eastmoon@gmail.com>
2015-10-15 15:02:17 +0800

    20151015
    DLL Create and Using.

---
+++ Tutorial-ClassLibrary/Client/Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMethods;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // default Plan
+            Console.WriteLine("Default :");
+            MyMathPlan plan = new MyMathPlan();
+            plan.Execute();
+
+            // Reset plan
+            Console.WriteLine("New :");
+            plan = new NewMathPlan();
+            plan.Execute();
+        }
+    }
+
+    class NewMathPlan : MyMathPlan
+    {
+        public override void Execute()
+        {
+            this.ResetAccumulate(1);
+
+            for(int i = 0 ; i < 10 ; i++ )
+            {
+                this.Accumulate(i);
+                Console.WriteLine(this.GetAccumulate());
+            }
+        }
+    }
+}
---
+++ Tutorial-ClassLibrary/Library/Library/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMethods
+{
+    public class MyMath
+    {
+        private int m_accumulateNumber;
+        public void ResetAccumulate( int a_number = 0 )
+        {
+            this.m_accumulateNumber = a_number;
+        }
+        public void Accumulate(int a_number)
+        {
+            this.m_accumulateNumber += a_number;
+        }
+        public int GetAccumulate()
+        {
+            return this.m_accumulateNumber;
+        }
+
+        public static int AddNumber( int a_num1, int a_num2 )
+        {
+            return a_num1 + a_num2;
+        }
+    }
+
+    public class MyMathPlan : MyMath
+    {
+        public virtual void Execute()
+        {
+            // Reset
+            this.ResetAccumulate();
+            // Accumulate
+            this.Accumulate(1);
+            // Output
+            Console.WriteLine(this.GetAccumulate());
+        }
+    }
+}
